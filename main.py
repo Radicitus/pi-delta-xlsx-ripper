fa96be88d6c0d84e6c931b3831c290f7c1b54153
Radicitus <novivolatus@gmail.com>
2020-12-07 00:28:19 -0800

    Added functionality to write to a json file titled "brother_info.json"

--- main.py
+++ main.py
@@ -16,14 +16,33 @@ maxRow = ws1.max_row
 maxCol = ws1.max_column
 
 # Set up JSON dict
-json_data = {}
+json_data = {"brothers": []}
 
 # Rip data and add to JSON dict
 #   Headers: ID, Name (First Last), Class, Active (Y or N), LinkedIn, Major, Cabby/Exec (Y or N), Profile URL
 for r in range(2, maxRow + 1):
+    brother = {}
     for c in range(1, maxCol + 1):
         print(ws1.cell(row=r, column=c).value)
-
+        if c == 1:
+            brother["id"] = ws1.cell(row=r, column=c).value
+        if c == 2:
+            brother["name"] = ws1.cell(row=r, column=c).value
+        if c == 3:
+            brother["class"] = ws1.cell(row=r, column=c).value
+        if c == 4:
+            brother["active_status"] = ws1.cell(row=r, column=c).value
+        if c == 5:
+            brother["linkedin_url"] = ws1.cell(row=r, column=c).value
+        if c == 6:
+            brother["major"] = ws1.cell(row=r, column=c).value
+        if c == 7:
+            brother["cabby_exec_status"] = ws1.cell(row=r, column=c).value
+        if c == 8:
+            brother["profile_url"] = ws1.cell(row=r, column=c).value
+        if c > 8:
+            print("ERROR: Column is " + str(c) + ", Cell Value is " + str(ws1.cell(row=r, column=c).value))
+    json_data["brothers"].append(brother)
 
 # Write JSON file
 with open("brother_info.json", "w") as outfile:
